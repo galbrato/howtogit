58d05dc0f6d14d936d0e6affa2eebb7a902ef8f8
Marcos Camargo <marcos.cesar.camargo@usp.br>
2016-04-10 19:16:44 -0300

    calculando uns cosceno loco aqui mano

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+double fat(double a){
+    double res = 1;
+    while(a--) res *= a;
+    return res;
+}
 
+double pot(double a, double n){
+    double res = 1;   
+    while(n--) res *= a;
+    return res;
+}
+
+double cos(double x){
+	double res = 1;
+	int i;
+	for(i = 0; i < 10; i++){
+		int f = 2*i;
+		 res += (i% 2 == 0) ? (pot(x, f)/fat(f)) : -(pot(x, f)/fat(f));
+	}
+}
 
 int main (int argc, char const *argv[]){
 		
 	printf("Ola mundo!!\n");		
-	printf("OI CARTAZSSZ!!\n");		
+	printf("cos(3.14) = %lf\n", cos(3.14));
 
 	return 0;
 }
